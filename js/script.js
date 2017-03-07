336f79ba9a4e453fcf4b48eb708b4c965f12335d
Unknown <sejui@hotmail.com>
2017-03-06 17:15:27 -0800

    homepage done + bagpack done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
 "use strict";
+var windowW = document.documentElement.clientWidth;
+console.log(windowW);
 
-
-// toggel productlist 
+// toggel productlist on homepage
 function toggleProductListPanel(x){
 	var panel = document.getElementById(x);
 	var coverPanel = document.getElementById("coverPanel");
 	var maxW = "80%";
 
-
+	//if the screen size is smaller than 1024, the toggle panel will be 100% of screen size, 
+	//otherwise it will be 80% of screen size
+	if(document.documentElement.clientWidth < "1024"){
+		maxW="100%";
+	}else{
+		maxW="80%";
+	}	
 	if(panel.style.width == maxW){
 		panel.style.width = "0px";
 		coverPanel.style.display="none";
 		coverPanel.style.overflow = "hidden !important";
 	}else{
+		console.log(document.documentElement.clientWidth);
+	
 		panel.style.width = maxW;
 		coverPanel.style.display = "initial";
 		coverPanel.style.overflow = "scroll !important";
 	}
 }
 
-// toggel information
+// toggel information in product pages
 function toggleInfo(x){
 	var panel = document.getElementById(x);
 	var information = document.getElementById("info");
@@ -28,10 +37,43 @@ function toggleInfo(x){
 
 	if(information.style.height == maxH){
 		information.style.height = "0px";
-		// information.style.display="none";
 	}else{
 		information.style.height = maxH;
-		// information.style.display="initial";
 
 	}
 }
+
+
+// loading function
+// window.onload = function(){
+// 	setTimeout("loadingFunction()",2500);
+
+// }
+// document.getElementById("hideAll").style.display="none";
+// document.getElementById("loading").style.display="block";
+
+// function loadingFunction(){
+// 	document.getElementById("hideAll").style.display="block";
+// 	document.getElementById("loading").style.display="none";
+
+
+// 	document.getElementById("hideAll").style.animation="fadein 1s";
+// }
+
+
+// detect user scrolling section. when user reaches specific position, do something 
+$(".check").each(function(){
+	var pos = $(this).offset(),  //retrieve current position of an element relative to the document
+		wH = $(window).height,   //view port height
+		wY = $(window).scrollTop(); // get vertical position of the scroll bar to match an element
+	
+	if(pos.top<=wY+wH){
+		console.log("you reach the div!!!");
+		setTimeout(function(){
+			document.getElementsByClassName("overview2Content")[0].style.animation="fadein 2s";
+		},1500);
+		console.log(wY);
+		return wY;
+	}	
+
+});
